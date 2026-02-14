8eb4af2457885f37d1c844423563fc9139c06688
reza <rezaazizsimatupang@gmail.com>
2026-02-14 22:11:32 +0700

    vibe coding 2

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from '../features/auth/authSlice';
-import quranReducer from '../features/quran/quranSlice';
-import settingsReducer from '../features/settings/settingsSlice';
-import logReducer from '../features/logs/logSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/authSlice";
+import quranReducer from "../features/quran/quranSlice";
+import settingsReducer from "../features/settings/settingsSlice";
+import logReducer from "../features/logs/logSlice.js";
 
 export const store = configureStore({
   reducer: {
